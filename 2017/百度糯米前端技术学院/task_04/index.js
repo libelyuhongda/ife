526ca75d91e13ba6146713dcf426c656836622f0
jyz <1752995027@qq.com>
2017-07-20 09:15:44 +0800

    commit task_04

---
+++ 2017/百度糯米前端技术学院/task_04/index.js
@@ -0,0 +1,52 @@
+// 动态数据绑定（一）
+
+class Observer {
+  constructor(data) {
+    this.data = data
+    this.walk(data)
+  }
+
+  walk(obj) {
+    Object.keys(obj).forEach(key => {
+      this.convert(key, obj[key])
+    })
+  }
+
+  convert(key, val) {
+    if (val && typeof val == 'object') {
+      new Observer(val)
+    }
+
+    Object.defineProperty(this.data, key, {
+      enumerable: true,
+      configurable: true,
+      get: () => {
+        console.log('你访问了 ' + key)
+        return val
+      },
+      set: newVal => {
+        if (newVal === val) {
+          return
+        }
+        console.log('你设置了 ' + key + '，新的值为 ' + newVal)
+        val = newVal
+      }
+    })
+  }
+}
+
+let app1 = new Observer({
+  name: 'youngwind',
+  age: 25
+})
+
+let app2 = new Observer({
+  university: 'bupt',
+  major: 'computer'
+})
+
+// 要实现的结果如下：
+app1.data.name // 你访问了 name
+app1.data.age = 25  // 你设置了 age，新的值为 100
+app2.data.university // 你访问了 university
+app2.data.major = 'science'  // 你设置了 major，新的值为 science
